40ad2a1b2786d3b7b8482784e687c80cf974dfbe
Feras Albaroudi <feras.albaroudi@gmail.com>
2019-08-08 14:16:39 +1200

    Validate base

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ app.listen(port, () => {
     console.log("Listening on " + port);
 })
 
+app.get("/:base/:equation", (req, res) => {
+
+    let params = req.params;
+    // let base = parseFloat(params.base);
+
+    if (isNaN(params.base)) {
+        res.status(400).send();
+        return;
+    }
+
+    console.log(req.params);
+
+    res.status(200).json(req.params);
+})
+
